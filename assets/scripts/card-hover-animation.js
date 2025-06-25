39211e962ff436e80f1bdbca06474e5aed16566c
Thomas <thomassantospallarini@gmail.com>
2025-06-25 11:13:40 -0300

    feat: add mouseLeave eventListener

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,13 +1,18 @@
 function handleMouseEnter() {
-    this.classList.add("s-card--hovered");
+  this.classList.add("s-card--hovered");
+}
+
+function handleMouseLeave() {
+  this.classList.remove("s-card--hovered");
 }
 
 function addEventListenersToCards() {
   const cardElements = document.querySelectorAll(".s-card");
 
-  cardElements.forEach((card) =>
-    card.addEventListener("mouseenter", handleMouseEnter)
-  );
+  cardElements.forEach((card) => {
+    card.addEventListener("mouseenter", handleMouseEnter);
+    card.addEventListener("mouseleave", handleMouseLeave);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
